f73c59ed609407552703f7fe5cd3e63d5958534b
NAZIYASHAIK1695 <101784632+NAZIYASHAIK1695@users.noreply.github.com>
2022-09-02 10:47:48 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,13 @@
-resource "aws_s3_bucket" "inclusio-demo-naziya.shaik" {
-    bucket = "${var.bucket_name}" 
-    acl = "${var.acl_value}"   
+resource "aws_s3_bucket" "b" {
+  bucket = "inclusio-demo-naziya-shaik"
+
+  tags = {
+    Name        = "inclusio-demo-naziya-shaik"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
 }
